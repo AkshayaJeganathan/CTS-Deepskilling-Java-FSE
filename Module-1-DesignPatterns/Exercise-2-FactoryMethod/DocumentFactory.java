7b5daf87ed14cc3458b175991d86481b5de9685c
AkshayaJeganathan <akshayajeganathan5@gmail.com>
2026-06-14 20:56:39 +0530

    Update DocumentFactory.java

--- Module-1-DesignPatterns/Exercise-2-FactoryMethod/DocumentFactory.java
+++ Module-1-DesignPatterns/Exercise-2-FactoryMethod/DocumentFactory.java
@@ -1,21 +1,4 @@
-public class DocumentFactory {
+public abstract class DocumentFactory {
 
-    public static Document createDocument(String documentType) {
-
-        if (documentType.equalsIgnoreCase("WORD")) {
-            return new WordDocument();
-        }
-
-        else if (documentType.equalsIgnoreCase("PDF")) {
-            return new PdfDocument();
-        }
-
-        else if (documentType.equalsIgnoreCase("EXCEL")) {
-            return new ExcelDocument();
-        }
-
-        else {
-            throw new IllegalArgumentException("Invalid Document Type");
-        }
-    }
+    public abstract Document createDocument();
 }
