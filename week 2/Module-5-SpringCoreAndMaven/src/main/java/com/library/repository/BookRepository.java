5ae15bdbed0dc88b29babc5a2640cd09d8e80084
AkshayaJeganathan <akshayajeganathan5@gmail.com>
2026-06-21 11:45:32 +0530

    Added Week 2 Spring Core exercises 1, 2, 4, 5 and 7

---
+++ week 2/Module-5-SpringCoreAndMaven/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public void displayBookInfo() {
+        System.out.println("Book details retrieved successfully.");
+    }
+}
