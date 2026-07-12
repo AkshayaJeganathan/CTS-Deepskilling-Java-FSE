0ab99fa4c1d3d05767b88048943f02ba01f5fdaf
AkshayaJeganathan <akshayajeganathan5@gmail.com>
2026-07-12 12:10:27 +0530

    Completed Week 5 ReactJS HOL Hands-on 3 Score Calculator

---
+++ week 5/3. ReactJS-HOL Hands-on/scorecalculatorapp/src/App.js
@@ -0,0 +1,18 @@
+import CalculateScore from "./Components/CalculateScore";
+
+function App() {
+
+  return (
+
+    <CalculateScore
+      name="Akshaya Jeganathan"
+      school="ABC Higher Secondary School"
+      total={480}
+      goal={6}
+    />
+
+  );
+
+}
+
+export default App;
